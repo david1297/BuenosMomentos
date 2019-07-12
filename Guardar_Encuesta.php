108f24940b6a6d0ff4144d736b893dd24f5c8a31
david1297 <juandavid.andrade1997@gmail.com>
2019-07-12 17:21:03 -0500

    .

--- Guardar_Encuesta.php
+++ Guardar_Encuesta.php
@@ -2,9 +2,12 @@
   require_once ("config/db.php");
   require_once ("config/conexion.php");
   $IdEncuesta = $_POST['IdEncuesta'];
-  $sql=mysqli_query($con, "select LAST_INSERT_ID(NRes) as last,CURDATE() as Fecha,curTime() as Hora from resp  order by NRes desc limit 0,1 ");
-	$rw=mysqli_fetch_array($sql);
+  $sql=mysqli_query($con, "select LAST_INSERT_ID(NRes) as last from resp  order by NRes desc limit 0,1 ");
+  $rw=mysqli_fetch_array($sql);
   $Numero=$rw['last']+1;
+  $sql=mysqli_query($con, "select CURDATE() as Fecha,curTime() as Hora ");
+	$rw=mysqli_fetch_array($sql);
+ 
   $Fecha = $rw['Fecha'];
 	$Hora = $rw['Hora'];
   
@@ -15,14 +18,21 @@
     $Tipo=$row['Tipo'];
     $Pregunta=$row['Pregunta'];
     if ($Tipo=='Texto'){
-      echo $Pregunta.'<br>';
-      echo 'Respuesta:'.$_POST['T-'.$Id].'<br>';
+      
+      $Respuesta=$_POST['T-'.$Id];
+
+      $sql2 =  "INSERT INTO resp (Fecha,NRes,Pregunta,Respuesta,Hora) VALUES ('$Fecha',$Numero,'$Pregunta','$Respuesta','$Hora');";
+      $query_update = mysqli_query($con,$sql2);
+      echo $sql2;
     }else{
       if ($Tipo=='Seleccion'){
         if(!empty($_POST['S-'.$Id])){
-          echo $Pregunta.'<br>';
-          echo 'Respuesta:'.$_POST['S-'.$Id].'<br>';
-        } 
+          $Respuesta=$_POST['S-'.$Id];
+        } else{
+          $Respuesta='';
+        }
+        $sql2 =  "INSERT INTO resp (Fecha,NRes,Pregunta,Respuesta,Hora) VALUES ('$Fecha',$Numero,'$Pregunta','$Respuesta','$Hora');";
+        $query_update = mysqli_query($con,$sql2);
        
       }else{
         $sql1="SELECT * FROM p_seleccion where Pregunta =$Id ";
@@ -30,10 +40,14 @@
         while($row1=mysqli_fetch_array($query1)){
           $S=$row1['Id'];
           if(!empty($_POST['M-'.$S])){
-            echo $Pregunta.'<br>';
-            $Opcion=$row1['Opcion'];
-            echo $Opcion.'<br>';
+          
+            $Respuesta=$row1['Opcion'];
+            
+          }else{
+            $Respuesta='';
           }
+          $sql2 =  "INSERT INTO resp (Fecha,NRes,Pregunta,Respuesta,Hora) VALUES ('$Fecha',$Numero,'$Pregunta','$Respuesta','$Hora');";
+          $query_update = mysqli_query($con,$sql2);
         }
       }
     }
@@ -43,7 +57,6 @@
 
   }
   
-//  $sql =  "INSERT INTO respuestas (Respuestas,Fecha) VALUES ('$Respuesta',CURDATE());";
-//  $query_update = mysqli_query($con,$sql);
+//  
  
 ?>
